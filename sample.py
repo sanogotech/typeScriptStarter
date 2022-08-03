6bbd74bd741e497d60983ed9b49f506f05ef24a5
SANOGOTECH <sanogotech@yahoo.fr>
2022-08-03 10:26:12 +0000

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,19 @@
+ 
+class Person {
+    first_name: string;
+    last_name: string;
+  
+    constructor(fname: string, lname: string)
+    {
+        first_name = fname;
+        last_name = lname;
+    }
+    getName(): string
+    {
+        var fullname: string = first_name + last_name;
+        return fullname;
+    }
+}
+  
+var henry: Person;
 
